d19c31aec2fcdbb320f2f2e22c6033bd98d61b31
YasharGY <yasharagu@code.edu.az>
2023-07-20 12:31:36 +0400

    BE-009 IReadRepository created

---
+++ Fiorella/src/Core/Fiorella.Aplication/Abstraction/Repostiory/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Fiorella.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiorella.Aplication.Abstraction.Repostiory
+{
+	public interface IRepository<T> where T : BaseEntity, new()
+	{
+		public DbSet<T> Table { get; }
+	}
+}
