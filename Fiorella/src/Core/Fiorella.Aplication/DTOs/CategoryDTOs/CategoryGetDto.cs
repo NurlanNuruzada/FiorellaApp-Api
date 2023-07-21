72a3519be74715af0440b793eae6071b960e36fc
unknown <nurlangn@code.edu.az>
2023-07-21 06:03:03 +0400

    Solved Bug in Mapper

--- Fiorella/src/Core/Fiorella.Aplication/DTOs/CategoryDTOs/CategoryGetDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Fiorella.Aplication.DTOs.CategoryDTOs;
-
-public record CategoryGetDto(Guid Id, string name, string description);
