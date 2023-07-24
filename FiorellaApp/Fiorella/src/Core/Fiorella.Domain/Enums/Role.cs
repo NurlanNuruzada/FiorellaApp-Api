30f834e4cec383e921f05d121247969955f9ffef
YasharGY <yasharagu@code.edu.az>
2023-07-25 03:53:26 +0400

    Seeds is Created

---
+++ FiorellaApp/Fiorella/src/Core/Fiorella.Domain/Enums/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiorella.Domain.Enums
+{
+	public enum Role
+	{
+		Member,
+		Admin,
+		SuperAdmin
+		
+
+	}
+}
