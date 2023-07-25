f8d5680cf5261f6f8ae14e205143991a495f3a99
huseynovth <tahireh@code.edu.az>
2023-07-25 23:56:47 +0400

    Idedntity.Registr is done

---
+++ FiorellaApp/Fiorella/src/Core/Fiorella.Aplication/Validators/AuthValidators/RegisterDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using Fiorella.Aplication.DTOs.AuthDTOs;
+using FluentValidation;
+
+namespace Fiorella.Aplication.Validators.AuthValidators;
+
+public class RegisterDtoValidator:AbstractValidator<RegisterDto>
+{
+    public RegisterDtoValidator()
+    {
+        RuleFor(u => u.Fullname)
+            .MaximumLength(150);
+
+        RuleFor(u => u.email)
+            .EmailAddress()
+    .MaximumLength(255)
+        .NotEmpty()
+            .NotNull();
+
+        RuleFor(u => u.Fullname)
+            .MaximumLength(50)
+                .NotEmpty()
+            .NotNull();
+
+        RuleFor(u => u.Fullname)
+            .MaximumLength(150)
+                .NotEmpty()
+            .NotNull();
+    }   
+}
--- FiorellaApp/Fiorella/src/Infastructure/Fiorella.Persistence/Contexts/AppDbContext.cs
+++ FiorellaApp/Fiorella/src/Infastructure/Fiorella.Persistence/Contexts/AppDbContext.cs
@@ -1,18 +1,21 @@
 ﻿using Fiorella.Domain.Entities;
 using Fiorella.Persistence.Configurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fiorella.Persistence.Contexts;
 
-public class AppDbContext :DbContext
+public class AppDbContext :IdentityDbContext<AppUser>
+
 {
     public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
     {
     }
-    public DbSet<Category> Categories { get; set; } = null!;
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(CategoryConfiguration).Assembly);
         base.OnModelCreating(modelBuilder);
     }
+    public DbSet<Category> Categories { get; set; } = null!;
+    
 }
